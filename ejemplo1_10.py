56358e0ccd6b14b3969c9d07844c3588f571c44b
josselinereyes841 <josselinereyes35@outlook.es>
2019-09-17 17:10:42 -0500

    funcionando ejemplo1_10.py

---
+++ ejemplo1_10.py
@@ -0,0 +1,8 @@
+BASE = int(input("Escribe la base del rectangulo: "))
+ALTU = int(input("Escribe la altura: "))
+SUPER = BASE * ALTU
+PER = 2*(BASE + ALTU)
+print(f"Un rectangulo de base {BASE} y altura {ALTU} su superficie es {SUPER}")
+print(f"Un rectangulo de base {BASE} y altura {ALTU} su perimetro es {PER}")
+
+
