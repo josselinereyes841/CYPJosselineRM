d8f3da2633b2896d81fe936890d52c71aa9fa7e3
josselinereyes841 <josselinereyes35@outlook.es>
2019-09-22 20:40:31 -0500

    se suben

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,8 @@
+MAT = int(input("Numero de matricula: "))
+CAL1 = float(input("Ingrese la primera calificación: "))
+CAL2 = float(input("Ingresa la segunda calificación: "))
+CAL3 = float(input("Ingresa la tercera calificación: "))
+CAL4 = float(input("Ingresa la cuarta calificación: ")) 
+CAL5 = float(input("Ingresa la quinta calificación: "))
+PRO = (CAL1+CAL2+CAL3+CAL4+CAL5)/5
+print(f"El promedio de {MAT} es {PRO}")
