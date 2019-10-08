99fb79ab7d187fcbd10e61ce6382b2707179cf50
josselinereyes841 <54154767+josselinereyes841@users.noreply.github.com>
2019-10-08 17:50:19 -0500

    clase de 8 de octubre

---
+++ switch.py
@@ -0,0 +1,15 @@
+dia = int(input("Introduce un dia ente (1-5): "))
+if dia == 1:
+    print("Lunes")
+elif dia == 2:
+    print("Martes")
+elif dia == 3:
+    print("Miercoles")
+elif dia == 4:
+    print("Jueves")
+elif dia == 5:
+    print("Viernes")
+else: 
+    print("Opcion no valida")
+
+print("fin del programa!!")
