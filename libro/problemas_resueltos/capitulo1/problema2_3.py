f0a905e60b2073f46079d26b4ec0bd8716d365b5
josselinereyes841 <josselinereyes35@outlook.es>
2019-10-15 17:44:00 -0500

    15/10

--- libro/problemas_resueltos/capitulo1/problema2_3.py
+++ libro/problemas_resueltos/capitulo1/problema2_3.py
@@ -4,7 +4,7 @@ B = float(input("2: "))
 C = float(input("3: "))
 DIS =  B**2-4*A*C
 if DIS >= 0:
-    x1 = ((-B) + DIS**0.5)/2*A
-    x2 = ((-B) - DIS**0.5)/2*A
+    x1 = ((-B) + DIS**0.5)/(2*A)
+    x2 = ((-B) - DIS**0.5)/(2*A)
     print(f"x1 ={x1} , x2 ={x2}")
 print("Fin del programa ")
