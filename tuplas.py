97bf77a903f4786e849c393c7d19b23cc0619789
josselinereyes841 <josselinereyes35@outlook.es>
2019-11-05 17:40:27 -0600

    5/11/19

---
+++ tuplas.py
@@ -0,0 +1,7 @@
+#tuplas
+#se declaran con "("y")"
+#Son inmutables y organizan datos igual que las listas
+datos_db = ('132.248.173.88','root','12345678')
+print(datos_db[1])
+datos_db = ('132.248.173.88','admin','12345678')
+print(datos_db[1])
