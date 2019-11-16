f6fc2afe771e765d08ff24bacef8eef978343f0a
josselinereyes841 <josselinereyes35@outlook.es>
2019-11-15 23:47:39 -0600

    tarea

---
+++ color.py
@@ -0,0 +1,3 @@
+print(chr(27)+"[1;33m"+"Universidad Nacional Autonoma de Mexico") 
+print("\x1b[1;34m"+"Facltad de Estudios Superiores Aragon.") 
+print("\x1b[1;32m"+"Reyes Marquez Josseline Esmeralda") 
---
+++ color2.py
@@ -0,0 +1,3 @@
+print(chr(27)+"[1;33;47m"+"Universidad Nacional Autonoma de Mexico."+'\033[0;m') 
+print("\x1b[1;34;47m"+"Facltad de Estudios Superiores Aragon."+'\033[0;m') 
+print("\x1b[1;32;47m"+"Reyes Marquez Josseline Esmeralda."+'\033[0;m') 
